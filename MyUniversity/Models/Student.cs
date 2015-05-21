4315d9ece967d3fbc17d061b34e7a9d05abebd3a
mantu_hm <manas.pattanaik@hotmail.com>
2015-05-21 19:50:06 +0530

    -- decorated attributes for Student model.

--- MyUniversity/Models/Student.cs
+++ MyUniversity/Models/Student.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyUniversity.Models
 {
     public class Student
     {
         public int StudentID { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
-
 
 }
