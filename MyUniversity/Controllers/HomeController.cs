72826964f9046748f58ae6c122cc5fdecb74a4d2
mantu_hm <manas.pattanaik@hotmail.com>
2015-05-21 17:03:33 +0530

    -- first commit of university project

--- MyUniversity/Controllers/HomeController.cs
+++ MyUniversity/Controllers/HomeController.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using MyUniversity.DAL;
+using MyUniversity.ViewModels;
 
 namespace MyUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to Contoso University";
@@ -17,9 +18,15 @@ namespace MyUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            var data = from student in db.Students
+                       group student by student.EnrollmentDate into dateGroup
+                       select new EnrollmentDateGroup()
+                       {
+                           EnrollmentDate = dateGroup.Key,
+                           StudentCount = dateGroup.Count()
+                       };
 
-            return View();
+            return View(data);
         }
 
         public ActionResult Contact()
